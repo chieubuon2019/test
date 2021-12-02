26d9c9341a83ca2763fb1a4ad0f2976e7a99c6f2
chieubuon2019 <51229821+chieubuon2019@users.noreply.github.com>
2021-12-02 13:43:31 +0700

    update code

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log(" .......... HELLO WORLD ............ .....+++  ...", count);
+
+const parseJSON = strJSON => {
+    try {
+      if (strJSON) {
+        return JSON.parse(strJSON);
+      }
+    } catch (error) {
+      console.error('utility -> parseJSON -> try error', error);
+    }
+  
+    return '';
+  };
+  const getVersionGame = () => {
+    let version = process.env.FISH_VERSION || 1;
+    try {
+      version = parseInt(version);
+    } catch(ex) {
+      version = 1;
+    }
+    if (version === 1) version = '';
+  
+    return version;
+  };
+  
+  const isEmptyObject = (obj) => {
+    return !obj || !Object.keys(obj).length;
+  };
+  
+  const tryParseJson = strJSON => {
+    try {
+      const jsonObject = JSON.parse(strJSON);
+      return [undefined, jsonObject];
+    } catch (error) {
+      console.error('utility -> parseJSON -> try error', error);
+      return [error];
+    }
+  };
+  
+  
+  module.exports = {
+    parseJSON,
+    getVersionGame,
+    tryParseJson,
+    isEmptyObject
+  };
+  
