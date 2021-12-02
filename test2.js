b7d6c8732dcf7150a28f3d7d4e3067159e9dcf04
chieubuon2019 <51229821+chieubuon2019@users.noreply.github.com>
2021-12-02 16:00:06 +0700

    update test 2

---
+++ test2.js
@@ -0,0 +1,52 @@
+
+const parseJSON = strJSON => {
+    try {
+      if (strJSON) {
+        return JSON.parse(strJSON);
+      }
+    } catch (error) {
+      console.error('utility -> parseJSON -> try error', error);
+    }
+  
+    return '';
+  };
+  
+  const getVersionGame = () => {
+    return "1.1";
+  };
+  
+  const isEmptyObject = (obj) => {
+    return !obj || !Object.keys(obj).length;
+  };
+  
+  const tryParseJson = strJSON => {
+    try {
+      const jsonObject = JSON.parse(strJSON);
+      return [undefined, jsonObject];
+    } catch (error) {
+      console.error('utility -> parseJSON -> try error', error);
+      return [error];
+    }
+  };
+  
+  const main = (objectAssigned) => {
+    let listKeys = Object.keys(ExportedFunction);
+    for (let i = 0; i < listKeys.length; i++) {
+      const func = listKeys[i];
+      if (func != 'main') {
+        objectAssigned.prototype[func] = ExportedFunction[func];
+      }
+    }
+  }
+  
+  const ExportedFunction = {
+    parseJSON,
+    getVersionGame,
+    tryParseJson,
+    isEmptyObject,
+    main
+  }
+  
+  
+  module.exports = ExportedFunction;
+  
