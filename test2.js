04993dcf6be93f751790651c1b550b82a6377186
chieubuon2019 <51229821+chieubuon2019@users.noreply.github.com>
2021-12-17 17:04:32 +0700

    Update test2.js

--- test2.js
+++ test2.js
@@ -30,7 +30,7 @@ const parseJSON = strJSON => {
   };
   
   const main = (objectAssigned) => {
-    console.log("  --->>> goi ham Main -------!!dfdsfadsfa!!! fdsfaf333-->>>   ");
+    console.log("  --->>> goi ham Main -------!!dfdsfadsfa!!! fdsafadsfsaf-->>>   ");
   }
   
   const ExportedFunction = {
