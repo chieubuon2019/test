3af22cc44e35c18e2dd07301b74c1771c44403a4
chieubuon2019 <51229821+chieubuon2019@users.noreply.github.com>
2021-12-17 17:20:59 +0700

    Update test2.js

--- test2.js
+++ test2.js
@@ -30,7 +30,7 @@ const parseJSON = strJSON => {
   };
   
   const main = (objectAssigned) => {
-    console.log("  --->>> MAIN >  dfsfafasfsa ");
+    console.log("  --->>> MAIN >  dfsfafasfsa!!!! ");
   }
   
   const ExportedFunction = {
